b4385ad3af542d4f3e04dd66598674f92e52f158
Gwendoline-Soulard <gwendoline.soulard@gmail.com>
2023-06-09 13:41:45 +0200

    structure-database

---
+++ src/Entity/Apply.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ApplyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ApplyRepository::class)
+ */
+class Apply
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $date;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Candidate::class, inversedBy="applies")
+     */
+    private $candidate;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Job::class, inversedBy="applies")
+     */
+    private $Job;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getCandidate(): ?Candidate
+    {
+        return $this->candidate;
+    }
+
+    public function setCandidate(?Candidate $candidate): self
+    {
+        $this->candidate = $candidate;
+
+        return $this;
+    }
+
+    public function getJob(): ?Job
+    {
+        return $this->Job;
+    }
+
+    public function setJob(?Job $Job): self
+    {
+        $this->Job = $Job;
+
+        return $this;
+    }
+}
