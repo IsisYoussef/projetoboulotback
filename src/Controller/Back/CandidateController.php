e24e92daa6ab3a673fe3f6a68c4b531cb7964091
Gwendoline-Soulard <gwendoline.soulard@gmail.com>
2023-06-15 15:04:21 +0200

    CRUD dashboard controllers

---
+++ src/Controller/Back/CandidateController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Controller\Back;
+
+use App\Entity\Candidate;
+use App\Form\CandidateType;
+use App\Repository\CandidateRepository;
+use Datetime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+/**
+ * @Route("/back/candidate", name="app_back_candidate")
+ */
+class CandidateController extends AbstractController
+{
+    /**
+     * @Route("/", name="candidate_index", methods={"GET"})
+     * 
+     */
+    public function index(CandidateRepository $candidateRepository): Response
+    {
+        return $this->render('back/candidate/index.html.twig', [
+            'candidates' => $candidateRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="candidate_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, CandidateRepository $candidateRepository): Response
+    {
+
+        $candidate = new Candidate();
+        $form = $this->createForm(CandidateType::class, $candidate);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $candidateRepository->add($candidate, true);
+
+            return $this->redirectToRoute('app_back_candidate_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/candidate/new.html.twig', [
+            'candidate' => $candidate,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="candidate_show", methods={"GET"})
+     */
+    public function show(Candidate $candidate): Response
+    {
+        if ($candidate === null){throw $this->createNotFoundException("ce candidat n'existe pas");}
+
+        return $this->render('back/candidate/show.html.twig', [
+            'candidate' => $candidate,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="candidate_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, ?Candidate $candidate, CandidateRepository $candidateRepository): Response
+    {
+
+        $form = $this->createForm(CandidateType::class, $candidate);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $candidateRepository->add($candidate, true);
+
+            return $this->redirectToRoute('app_back_candidate_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('back/candidate/edit.html.twig', [
+            'candidate' => $candidate,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="candidate_delete", methods={"POST"})
+     */
+    public function delete(Request $request, ?Candidate $candidate, CandidateRepository $candidateRepository): Response
+    {
+        if ($candidate === null){throw $this->createNotFoundException("ce candidat n'existe pas");}
+        if ($this->isCsrfTokenValid('delete'.$candidate->getId(), $request->request->get('_token'))) {
+            $candidateRepository->remove($candidate, true);
+        }
+
+        return $this->redirectToRoute('app_back_candidate_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/UserType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('phone')
+            ->add('address')
+            ->add('postalCode')
+            ->add('city')
+            ->add('presentation')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('gender')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
