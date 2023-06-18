b91bee025cb5454f8749a03609541de1c74ce0d3
IsisYoussef <isisyoussef24@gmail.com>
2023-06-18 16:21:31 +0200

    delete easyadmin

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
