1ec44d304f8c49b27eec43a56cc24ca2b7d2cfbe
Gwendoline-Soulard <gwendoline.soulard@gmail.com>
2023-06-27 15:14:47 +0200

    update logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,9 +14,9 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+        return $this->redirectToRoute('dashboard');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -27,9 +27,9 @@ class SecurityController extends AbstractController
     }
 
     /**
-     * @Route("api/logout", name="app_logout")
+     * @Route("/logout", name="app_logout")
      */
-    public function logout(): void
+    public function logout()
     {
         //throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
